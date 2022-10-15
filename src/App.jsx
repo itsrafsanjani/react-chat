4c5ab27588e294b80da7000c4660ae8f90c6c6ac
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-10-15 20:10:36 +0600

    feat: socket io connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+import socketIO from 'socket.io-client'
+const socket = socketIO.connect(import.meta.env.VITE_API_URL)
+
 function App() {
-return (
-    <div className="App">
-        Hello World
-    </div>
-  )
+  return <div className='App'>Hello World</div>
 }
 
 export default App

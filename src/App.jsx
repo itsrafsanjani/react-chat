19851fbe4fa70af561439b2a03c5812387c4f37c
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-10-15 22:00:22 +0600

    fix: env variable call

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import socketIO from 'socket.io-client'
 const socket = socketIO.connect(import.meta.env.VITE_API_URL)
 
 function App() {
-  return <div className='App'>Hello World</div>
+  return <div className='App'>Hello World {import.meta.env.VITE_API_URL}</div>
 }
 
 export default App

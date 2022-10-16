314ec4b3d6b47656643947c0d5ab7423fd497ddf
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-10-16 22:36:00 +0600

    feat: working

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import socketIO from 'socket.io-client'
-const socket = socketIO.connect(import.meta.env.VITE_API_URL)
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import ChatPage from './components/ChatPage';
+import socketIO from 'socket.io-client';
 
+const socket = socketIO.connect(import.meta.env.VITE_API_URL)
 function App() {
-  return <div className='App'>Hello World {import.meta.env.VITE_API_URL}</div>
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

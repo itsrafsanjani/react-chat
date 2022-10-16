4c87171de7dba2357c76bbc1de95c48b44126ffd
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-10-16 22:59:53 +0600

    feat: tailwind css added and open sans font used

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import ChatPage from './components/ChatPage';
-import socketIO from 'socket.io-client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import ChatPage from './components/ChatPage'
+import socketIO from 'socket.io-client'
 
 const socket = socketIO.connect(import.meta.env.VITE_API_URL)
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className='font-sans'>
         <Routes>
-          <Route path="/" element={<Home socket={socket} />}></Route>
-          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+          <Route path='/' element={<Home socket={socket} />}></Route>
+          <Route path='/chat' element={<ChatPage socket={socket} />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
